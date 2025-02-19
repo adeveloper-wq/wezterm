321f50a088bb7596b52612cbd7aaa2dcb994eae9
adeveloper-wq <dajesch@gmail.com>
2025-02-19 08:05:54 +0100

    Add jump words config and new theme

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Latte'
+config.color_scheme = 'Gruvbox light, hard (base16)'
 
 config.font_size = 18.0
 
@@ -12,5 +12,12 @@ config.scrollback_lines = 20000
 config.send_composed_key_when_left_alt_is_pressed = true
 
 config.native_macos_fullscreen_mode = true
+
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
 
 return config
